10e0b313f00d9484d877cb86dba8acafb5245f5f
MaxG <113623110+MaxSUD84@users.noreply.github.com>
2024-04-04 01:19:16 +0300

    ADD: mysql & some files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import './style.css'
+// import App from './App.vue'
+
+const App = {
+    data() {
+        return {
+            counter: 42
+        }
+    }
+}
 
 createApp(App).mount('#app')
