0c3fe1b3f79655f7cdd318db61e1d418ba3639eb
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2024-04-04 18:17:57 +0300

    EDT: teach some methods

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 // import './style.css'
 // import App from './App.vue'
 
 const App = {
     data() {
         return {
-            counter: 42
-        }
-    }
-}
+            placeholderString: "Введие название заметки",
+            title: "Список заметок",
+            inputValue: "",
+            notes: ["Заметка 1", "Заметка 2"],
+        };
+    },
+    methods: {
+        inputChangeHandler(event: InputEvent) {
+            // console.log(event.target.value);
+            this.inputValue = event.target.value;
+        },
+        btnAddNoteHandler() {
+            if (this.inputValue) {
+                this.notes.push(this.inputValue);
+            }
+            this.inputValue = "";
+        },
+        // Timing : 51:13
+    },
+};
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
